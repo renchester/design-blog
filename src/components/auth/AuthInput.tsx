282b814880961dc7de0754512f4c9ac1350b7eb1
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-16 03:27:00 +0800

    Add initial auth components and styles

---
+++ src/components/auth/AuthInput.tsx
@@ -0,0 +1,76 @@
+import './AuthInput.scss';
+
+type AuthInputProps = {
+  id: string;
+  type: string;
+  name: string;
+  label: string;
+  value: string;
+  isValid?: boolean;
+  placeholder?: string;
+  handleChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  errorMessage?: string;
+  minLength?: number;
+  maxLength?: number;
+  isRequired?: boolean;
+};
+
+function AuthInput(props: AuthInputProps) {
+  const {
+    id,
+    type,
+    name,
+    label,
+    value,
+    placeholder,
+    handleChange,
+    isValid,
+    errorMessage,
+    minLength,
+    maxLength,
+    isRequired,
+  } = props;
+
+  return (
+    <div className="auth-input">
+      <label htmlFor={id} className="auth-input__label">
+        {label}
+      </label>
+      <input
+        id={id}
+        type={type}
+        name={name}
+        className="auth-input__input"
+        onChange={handleChange}
+        placeholder={placeholder || ''}
+        required={isRequired || false}
+        minLength={minLength || -1}
+        maxLength={maxLength || -1}
+      />
+
+      {/* Display validation status as icons */}
+      {value.length > 0 &&
+        isValid !== undefined &&
+        (isValid ? (
+          <div className="auth-validation" aria-label={`${label} is valid`}>
+            <i className="material-symbols-outlined auth-validation__icon auth-validation__valid-icon">
+              check
+            </i>
+          </div>
+        ) : (
+          <div className="validation" aria-label={`${label} is invalid`}>
+            <i className="material-symbols-outlined auth-validation__icon auth-validation__invalid-icon">
+              close
+            </i>
+          </div>
+        ))}
+
+      {/* Display error messages if there are any */}
+      {errorMessage && !isValid && value.length > 0 && (
+        <p className="auth-input__error">{errorMessage}</p>
+      )}
+    </div>
+  );
+}
+
+export default AuthInput;
---
+++ src/components/auth/LoginForm.tsx
@@ -0,0 +1,94 @@
+'use client';
+
+import { useState } from 'react';
+import debounce from 'lodash.debounce';
+
+import AuthInput from './AuthInput';
+import validateEmail from '@/utils/validators/validateEmail';
+import validatePassword from '@/utils/validators/validatePassword';
+
+function LoginForm() {
+  const DEBOUNCE_TIME = 1000;
+
+  const [email, setEmail] = useState('');
+  const [isEmailValid, setEmailValidity] = useState(false);
+  const [emailError, setEmailError] = useState('');
+
+  const [password, setPassword] = useState('');
+  const [isPasswordValid, setPasswordValidity] = useState(false);
+  const [passwordError, setPasswordError] = useState('');
+
+  const isButtonDisabled = !(isEmailValid && isPasswordValid);
+
+  const handleEmailChange = debounce(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const inputValue = e.target.value;
+      setEmail(inputValue);
+
+      try {
+        const validity = validateEmail(inputValue);
+        setEmailValidity(validity);
+      } catch (error) {
+        if (error instanceof Error) {
+          setEmailValidity(false);
+          setEmailError(error.message);
+        }
+      }
+    },
+    DEBOUNCE_TIME,
+  );
+
+  const handlePasswordChange = debounce(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const inputValue = e.target.value;
+      setPassword(inputValue);
+
+      try {
+        const validity = validatePassword(inputValue);
+        setPasswordValidity(validity);
+      } catch (error) {
+        if (error instanceof Error) {
+          setPasswordValidity(false);
+          setPasswordError(error.message);
+        }
+      }
+    },
+    DEBOUNCE_TIME,
+  );
+
+  const handleEmailLogin = async () => {};
+
+  return (
+    <form action="" className="auth__form" onSubmit={handleEmailLogin}>
+      <AuthInput
+        id="login__email"
+        name="email"
+        type="email"
+        label="Email"
+        value={email}
+        isValid={isEmailValid}
+        handleChange={handleEmailChange}
+        errorMessage={emailError}
+      />
+      <AuthInput
+        id="login__password"
+        name="password"
+        type="password"
+        label="Password"
+        value={password}
+        isValid={isPasswordValid}
+        handleChange={handlePasswordChange}
+        errorMessage={passwordError}
+      />
+
+      <button
+        type="submit"
+        className="auth__btn-submit"
+        disabled={isButtonDisabled}
+      >
+        Log in
+      </button>
+    </form>
+  );
+}
+export default LoginForm;
