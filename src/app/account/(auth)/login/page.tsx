caa50485cae3a2d6d250241bef3db8eb97b13d85
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-17 06:36:44 +0800

    Move signup and login pages into own route group

---
+++ src/app/account/(auth)/login/page.tsx
@@ -0,0 +1,38 @@
+import '../AuthPage.scss';
+import LoginForm from '@/components/auth/LoginForm';
+import Link from 'next/link';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Log in',
+};
+
+function LoginPage() {
+  return (
+    <>
+      <section className="auth-page__details">
+        <h2 id="login-title" className="auth-page__title">
+          Welcome back to Dezien!
+        </h2>
+        <p className="auth-page__terms">
+          By continuing, you agree to our{' '}
+          <span className="auth-page__link">User Agreement</span> and{' '}
+          <span className="auth-page__link">Privacy Policy</span>.
+        </p>
+      </section>
+
+      <section className="auth-page__auth">
+        <LoginForm />
+      </section>
+
+      <p className="auth-page__other">
+        Don&apos;t have an account yet?
+        <Link href="/account/signup" className="auth-page__link">
+          {' '}
+          Sign up now
+        </Link>
+      </p>
+    </>
+  );
+}
+export default LoginPage;
---
+++ src/app/account/(auth)/signup/page.tsx
@@ -0,0 +1,38 @@
+import '../AuthPage.scss';
+import SignupForm from '@/components/auth/SignupForm';
+import Link from 'next/link';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Sign up',
+};
+
+function RegisterPage() {
+  return (
+    <>
+      <section className="auth-page__details">
+        <h2 id="login-title" className="auth-page__title">
+          Welcome to Dezien!
+        </h2>
+        <p className="auth-page__terms">
+          By continuing, you are creating a Dezien account and hereby agree to
+          our <span className="auth-page__link">User Agreement</span> and{' '}
+          <span className="auth-page__link">Privacy Policy</span>.
+        </p>
+      </section>
+
+      <section className="auth-page__auth">
+        <SignupForm />
+      </section>
+
+      <p className="auth-page__other">
+        Already have an account?
+        <Link href="/account/login" className="auth-page__link">
+          {' '}
+          Login
+        </Link>
+      </p>
+    </>
+  );
+}
+export default RegisterPage;
