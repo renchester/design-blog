ac8029248112c50a950c4fb96cc3bdec747bbe20
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-17 06:38:04 +0800

    Update AuthInput styles

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -36,34 +36,35 @@ function AuthInput(props: AuthInputProps) {
       <label htmlFor={id} className="auth-input__label">
         {label}
       </label>
-      <input
-        id={id}
-        type={type}
-        name={name}
-        className="auth-input__input"
-        onChange={handleChange}
-        placeholder={placeholder || ''}
-        required={isRequired || false}
-        minLength={minLength || -1}
-        maxLength={maxLength || -1}
-      />
-
-      {/* Display validation status as icons */}
-      {value.length > 0 &&
-        isValid !== undefined &&
-        (isValid ? (
-          <div className="auth-validation" aria-label={`${label} is valid`}>
-            <i className="material-symbols-outlined auth-validation__icon auth-validation__valid-icon">
-              check
-            </i>
-          </div>
-        ) : (
-          <div className="validation" aria-label={`${label} is invalid`}>
-            <i className="material-symbols-outlined auth-validation__icon auth-validation__invalid-icon">
-              close
-            </i>
-          </div>
-        ))}
+      <div className="auth-input__input-wrapper">
+        <input
+          id={id}
+          type={type}
+          name={name}
+          className="auth-input__input"
+          onChange={handleChange}
+          placeholder={placeholder || ''}
+          required={isRequired || false}
+          minLength={minLength || -1}
+          maxLength={maxLength || -1}
+        />
+        {/* Display validation status as icons */}
+        {value.length > 0 &&
+          isValid !== undefined &&
+          (isValid ? (
+            <div className="auth-validation" aria-label={`${label} is valid`}>
+              <i className="material-symbols-outlined auth-validation__icon auth-validation__valid-icon">
+                check
+              </i>
+            </div>
+          ) : (
+            <div className="auth-validation" aria-label={`${label} is invalid`}>
+              <i className="material-symbols-outlined auth-validation__icon auth-validation__invalid-icon">
+                close
+              </i>
+            </div>
+          ))}
+      </div>
 
       {/* Display error messages if there are any */}
       {errorMessage && !isValid && value.length > 0 && (
