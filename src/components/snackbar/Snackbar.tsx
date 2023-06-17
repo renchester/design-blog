7feccc12e0e68646bfe95811b1ecb0dadde1b448
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-18 04:41:06 +0800

    Add Snackbar component and styles

---
+++ src/components/snackbar/Snackbar.tsx
@@ -0,0 +1,77 @@
+import './Snackbar.scss';
+import { Alert } from '@/types/types';
+
+type SnackbarProps = {
+  alert: Alert;
+};
+
+const successIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="30"
+    height="30"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="#ffffff"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="snackbar__icon"
+  >
+    <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+    <polyline points="22 4 12 14.01 9 11.01"></polyline>
+  </svg>
+);
+
+const failIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="30"
+    height="30"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="#ffffff"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="snackbar__icon"
+  >
+    <circle cx="12" cy="12" r="10"></circle>
+    <line x1="15" y1="9" x2="9" y2="15"></line>
+    <line x1="9" y1="9" x2="15" y2="15"></line>
+  </svg>
+);
+
+const infoIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="30"
+    height="30"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="#ffffff"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="snackbar__icon"
+  >
+    <circle cx="12" cy="12" r="10"></circle>
+    <line x1="12" y1="16" x2="12" y2="12"></line>
+    <line x1="12" y1="8" x2="12.01" y2="8"></line>
+  </svg>
+);
+
+function Snackbar(props: SnackbarProps) {
+  const { alert } = props;
+
+  return (
+    <div role="alert" className="snackbar" data-status={alert.status}>
+      {alert.status === 'error' && failIcon}
+      {alert.status === 'success' && successIcon}
+      {alert.status === 'neutral' && infoIcon}
+      <p className="snackbar__message">{alert.message}</p>
+    </div>
+  );
+}
+
+export default Snackbar;
