9ed289c9dff39c7eceedbc64181741dd628c79b6
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-21 06:22:02 +0800

    Set font preload properties to false

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -11,6 +11,7 @@ export const inter = Inter({
   subsets: ['latin', 'latin-ext'],
   variable: '--font-inter',
   display: 'swap',
+  preload: false,
 });
 
 export const roboto = Roboto({
@@ -18,24 +19,28 @@ export const roboto = Roboto({
   weight: ['300', '400', '700', '900'],
   variable: '--font-roboto',
   display: 'swap',
+  preload: false,
 });
 
 export const raleway = Raleway({
   subsets: ['latin', 'latin-ext'],
   variable: '--font-raleway',
   display: 'swap',
+  preload: false,
 });
 
 export const montserrat = Montserrat({
   subsets: ['latin', 'latin-ext'],
   variable: '--font-montserrat',
   display: 'swap',
+  preload: false,
 });
 
 export const playfair = Playfair_Display({
   subsets: ['latin', 'latin-ext'],
   variable: '--font-playfair',
   display: 'swap',
+  preload: false,
 });
 
 export const poppins = Poppins({
@@ -43,4 +48,5 @@ export const poppins = Poppins({
   weight: ['400', '500', '600', '700'],
   variable: '--font-poppins',
   display: 'swap',
+  preload: false,
 });
