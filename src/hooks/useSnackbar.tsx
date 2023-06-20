c5eea83d242105590524d0b6ad27fe762cb0feab
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-21 06:25:39 +0800

    Separate snackbar global context from custom hook

--- src/hooks/useSnackbar.tsx
+++ src/hooks/useSnackbar.tsx
@@ -1,62 +1,9 @@
 'use client';
 
-import { Alert } from '@/types/types';
-import {
-  ReactNode,
-  createContext,
-  useCallback,
-  useContext,
-  useEffect,
-  useMemo,
-  useState,
-} from 'react';
-import Snackbar from '@/components/snackbar/Snackbar';
+import SnackbarContext from '@/context/SnackbarContext';
+import { useContext } from 'react';
 
-type ContextType = {
-  addAlert: (content: Alert) => void;
-};
-
-type SnackbarProviderProps = {
-  children: ReactNode;
-};
-
-const SnackbarContext = createContext<ContextType | null>(null);
-
-export const SnackbarProvider = (props: SnackbarProviderProps) => {
-  const { children } = props;
-  const [alerts, setAlerts] = useState<Alert[]>([]);
-
-  const AUTO_DISMISS = 4500;
-  const activeAlertIds = alerts.join(',');
-
-  useEffect(() => {
-    if (activeAlertIds.length > 0) {
-      const snackbarTimer = setTimeout(
-        () => setAlerts((alerts) => alerts.slice(0, alerts.length - 1)),
-        AUTO_DISMISS,
-      );
-      return () => clearTimeout(snackbarTimer);
-    }
-  }, [activeAlertIds]);
-
-  const addAlert = useCallback(
-    (content: Alert) => setAlerts((alerts) => [content, ...alerts]),
-    [],
-  );
-
-  const value = useMemo(() => ({ addAlert }), [addAlert]);
-
-  return (
-    <SnackbarContext.Provider value={value}>
-      {children}
-      {alerts.map((alert) => (
-        <Snackbar key={alert.message} alert={alert} />
-      ))}
-    </SnackbarContext.Provider>
-  );
-};
-
-export const useSnackbar = () => {
+const useSnackbar = () => {
   const context = useContext(SnackbarContext);
 
   if (context === null || context === undefined) {
@@ -65,3 +12,5 @@ export const useSnackbar = () => {
 
   return context;
 };
+
+export default useSnackbar;
