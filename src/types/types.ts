90bb033fe7bd21f6d11e9cfbc814c3dc3fe83f11
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-23 13:17:40 +0800

    Update app types

--- src/types/types.ts
+++ src/types/types.ts
@@ -13,5 +13,52 @@ export interface User {
   email: string;
   first_name: string;
   last_name: string;
-  admin?: boolean;
+  is_admin: boolean;
+  is_verified_author: boolean;
 }
+
+export interface BlogPost {
+  _id: string;
+  date_created: Date;
+  title: string;
+  slug: string;
+  author: User;
+  editors: User[];
+  content: string;
+  comments: Comment[];
+  liked_by: User[];
+  tags: Tag[];
+  edits: {
+    timestamp: Date;
+  }[];
+  is_private: boolean;
+  category: BlogCategory;
+  display_img: {
+    url: string;
+    owner: string;
+    source: string;
+  };
+}
+
+export interface Comment {
+  _id: string;
+  date_created: Date;
+  author: User;
+  content: string;
+  comment_level: number;
+  parent_comment_id: string;
+  liked_by: User[];
+  edits: {
+    timestamp: Date;
+  }[];
+}
+
+export interface Tag {
+  _id: string;
+  name: string;
+}
+
+export type Alert = {
+  message: string;
+  status: 'success' | 'error' | 'neutral';
+};
