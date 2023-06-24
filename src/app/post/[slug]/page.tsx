e0d5dee388ddb64162dfbb23a181253c0f61f0fb
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-24 13:19:08 +0800

    Update PostPage content and styles

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,9 +1,15 @@
 import './PostPage.scss';
-import axios from '@/lib/axios';
-import { BlogPost } from '@/types/types';
-import { format } from 'date-fns';
 import Link from 'next/link';
 import unescape from 'validator/lib/unescape';
+import { format } from 'date-fns';
+
+import { CommentProvider } from '@/context/CommentContext';
+import SmallPreview from '@/components/blogPreview/SmallPreview';
+import axios from '@/lib/axios';
+import { BlogPost } from '@/types/types';
+
+import sampleBlogPost from '@/data/sampleBlogPost';
+import CommentFeed from '@/components/comments/CommentFeed';
 
 type PostPageProps = {
   params: {
@@ -17,73 +23,25 @@ const getBlogPost = async (slug: string) => {
   return post;
 };
 
-export async function generateStaticParams() {
-  const response = await axios.get('/api/posts');
-  const posts = response.data.posts as BlogPost[];
+// export async function generateStaticParams() {
+//   const response = await axios.get('/api/posts');
+//   const posts = response.data.posts as BlogPost[];
 
-  return posts.map((post) => ({
-    slug: post.slug,
-  }));
-}
-
-const JSON_data = {
-  display_img: {
-    url: 'https://images.unsplash.com/photo-1685777122043-8185dd1fa6d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-    owner: 'Alessio Zappatore',
-    source: 'Unsplash',
-  },
-  _id: '6481da04f9cdeca3d7ee0dda',
-  date_created: '2023-06-08T13:35:48.370Z',
-  title: ' Lorem ipsum facere eniet ab ipsum vel , placeat aperiam amet ',
-  slug: 'hello',
-  author: {
-    is_admin: false,
-    is_verified_author: false,
-    _id: '64807adcd2abd38380389b36',
-    username: 'renchester',
-    email: 'chesterramos5699@gmail.com',
-    first_name: 'Renchester',
-    last_name: 'ramos',
-  },
-  editors: [],
-  liked_by: [],
-  tags: [],
-  content:
-    ' Lorem ipsum dolor sit, amet consectetur adipisicing elit. In a facere eveniet ab ipsum vel beatae sed ad aliquid totam, placeat aperiam dignissimos doloremque iste, at amet eum eius? Minima!',
-  comments: [
-    {
-      date_created: '2023-06-09T15:58:44.871Z',
-      author: '64807adcd2abd38380389b36',
-      content: '&lt;sample&gt;&amp;&amp;&#x27; comment',
-      comment_level: 0,
-      liked_by: [],
-      edits: [],
-      _id: '64834c7459426fa4c9bff774',
-    },
-    {
-      date_created: '2023-06-09T16:01:15.908Z',
-      author: '64807adcd2abd38380389b36',
-      content: '&lt;sample&gt;&amp;&amp;&#x27; comment',
-      comment_level: 0,
-      liked_by: [],
-      edits: [],
-      _id: '64834cd406d186230163c132',
-    },
-  ],
-  edits: [],
-  category: 'architecture',
-  is_private: false,
-};
+//   return posts.map((post) => ({
+//     slug: post.slug,
+//   }));
+// }
 
 function PostPage(props: PostPageProps) {
   const { slug } = props.params;
   // const post = (await getBlogPost(slug)) as BlogPost;
-  const post = JSON_data;
+  const post = sampleBlogPost;
 
   const formattedDate = format(new Date(post.date_created), 'LL.dd.yy');
 
   return (
     <div className="post-page">
+      {/* META */}
       <article className="post-page__main">
         <Link
           href={`/category/${post.category}`}
@@ -103,11 +61,15 @@ function PostPage(props: PostPageProps) {
           |
           <span className="post-page__meta-info">
             {' '}
-            by {post.author.first_name} {post.author.last_name}
+            by{' '}
+            <Link href={`/user/${post.author.username}`}>
+              {post.author.first_name} {post.author.last_name}
+            </Link>
           </span>
         </div>
       </article>
 
+      {/* DISPLAY IMG */}
       <article className="post-page__display">
         <img
           src={post.display_img.url}
@@ -119,7 +81,72 @@ function PostPage(props: PostPageProps) {
         </span>
       </article>
 
-      <main className="post-page__content">{unescape(post.content)}</main>
+      {/* MAIN */}
+      <main className="post-page__content">
+        {unescape(post.content)}
+        {/* like post  */}
+      </main>
+
+      {/* TAGS */}
+      {post.tags.length > 0 && (
+        <section className="post-page__tags">
+          <h3 className="post-page__tags-title" id="post-page__tags-title">
+            Tags
+          </h3>
+
+          <ul className="post-page__tag-list">
+            {post.tags.map((tag) => (
+              <li key={`tag-${tag._id}`} className="post-page__tag">
+                {tag.name}
+              </li>
+            ))}
+          </ul>
+        </section>
+      )}
+
+      {/* COMMENTS */}
+      <section
+        className="post-page__comments"
+        aria-labelledby="post-page__comments-title"
+      >
+        <CommentProvider parentPost={post}>
+          <h3
+            id="post-page__comments-title"
+            className="post-page__comments-title"
+          >
+            Comments
+          </h3>
+          <CommentFeed comments={post.comments} />
+        </CommentProvider>
+      </section>
+
+      {/* FEATURED / LINKS TO OTHER POSTS */}
+      <section
+        className="post-page__featured"
+        aria-labelledby="post-page__featured-title"
+      >
+        <h3
+          id="post-page__featured-title"
+          className="post-page__featured-title"
+        >
+          Trending Posts
+        </h3>
+
+        <ul className="post-page__featured-list">
+          <li className="post-page__featured-item">
+            <SmallPreview blog={post} />
+          </li>
+          <li className="post-page__featured-item">
+            <SmallPreview blog={post} />
+          </li>
+          <li className="post-page__featured-item">
+            <SmallPreview blog={post} />
+          </li>
+          <li className="post-page__featured-item">
+            <SmallPreview blog={post} />
+          </li>
+        </ul>
+      </section>
     </div>
   );
 }
