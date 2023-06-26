b281850e0008f2216b34e3ed75079852dc3a2ea6
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-26 10:10:04 +0800

    Add Home Page sections and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import './HomePage.scss';
-import Hero from '@/components/hero/Hero';
 import { Metadata } from 'next';
+
+import sampleBlogPost from '@/data/sampleBlogPost';
+import Hero from '@/components/collections/Hero';
+import FeaturedTags from '@/components/collections/FeaturedTags';
+import FeaturedVideo from '@/components/collections/FeaturedVideo';
+import PostColumn from '@/components/collections/PostColumn';
+import CommunitySection from '@/components/collections/CommunitySection';
+import CallToAction from '@/components/collections/CallToAction';
 
 export const metadata: Metadata = {
   title: 'Dezien',
@@ -8,9 +15,37 @@ export const metadata: Metadata = {
 };
 
 export default function Home() {
+  const sampleBlogs = [sampleBlogPost, sampleBlogPost, sampleBlogPost];
+
   return (
     <main className="home-page">
-      <Hero />
+      <div className="home-page__section">
+        <Hero />
+      </div>
+
+      <div className="home-page__section-full">
+        <CallToAction />
+      </div>
+
+      <div className="home-page__section">
+        <PostColumn blogs={sampleBlogs} />
+      </div>
+
+      <div className="home-page__section-full">
+        <FeaturedTags tags={sampleBlogPost.tags} />
+      </div>
+
+      <div className="home-page__section">
+        <PostColumn blogs={sampleBlogs} />
+      </div>
+
+      <div className="home-page__section">
+        <FeaturedVideo />
+      </div>
+
+      <div className="home-page__section">
+        <CommunitySection />
+      </div>
     </main>
   );
 }
