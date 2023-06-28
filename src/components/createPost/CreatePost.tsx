6826435557aee7f773d4756551b9cdf6185e6111
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-28 11:47:50 +0800

    Add form tabs to Post Editor component

--- src/components/createPost/CreatePost.tsx
+++ src/components/createPost/CreatePost.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import './CreatePost.scss';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 import useAxiosInterceptors from '@/hooks/useAxiosInterceptors';
 import useAuth from '@/hooks/useAuth';
+import useSnackbar from '@/hooks/useSnackbar';
 import { BlogCategory } from '@/types/types';
 import PostPreview from './PostPreview';
 import TagPreview from './TagPreview';
@@ -37,12 +40,15 @@ export type AuthoredPost = {
 };
 
 function CreatePost() {
+  const router = useRouter();
   const { user } = useAuth();
+  const { addAlert } = useSnackbar();
   const axiosPrivate = useAxiosInterceptors();
 
   const initialPostState: AuthoredPost = {
-    title: 'Dezien Author',
-    content: '',
+    title: 'Dezien Editor Suite',
+    content:
+      'This is the preview page. You can use **markdown** to `create` your *post*.',
     author: 'Sean Silverico',
     category: 'art',
     display_img: {
@@ -55,7 +61,16 @@ function CreatePost() {
     is_private: false,
   };
 
-  const [postData, setPostData] = useState<AuthoredPost>(initialPostState);
+  const getInitialPostData = () => {
+    const fromLocal = localStorage.getItem('postData');
+
+    return fromLocal
+      ? (JSON.parse(fromLocal) as AuthoredPost)
+      : initialPostState;
+  };
+
+  const [postData, setPostData] = useState<AuthoredPost>(getInitialPostData());
+  const [activePanel, setActivePanel] = useState(1);
 
   const handleInfoChange = (
     e: React.ChangeEvent<
@@ -108,137 +123,285 @@ function CreatePost() {
     }));
   };
 
-  const handleSubmitPost = (e: React.FormEvent) => {
+  const handleSubmitPost = async (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (!user || !user.is_verified_author) return;
+
+    try {
+      const response = await axiosPrivate.post(`/api/posts`, postData);
+
+      console.log(response);
+
+      if (response.data.success) {
+        addAlert({
+          message: 'Successfully added post',
+          status: 'success',
+        });
+
+        // Reset post data
+        setPostData(initialPostState);
+
+        const { slug } = response.data.post.slug;
+
+        // Go to new post
+        slug && router.push(`/post/${slug}`);
+      } else {
+        throw new Error('Something went wrong.');
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+        addAlert({
+          message: 'Unable to create new post',
+          status: 'error',
+        });
+      }
+    }
   };
+
+  const isButtonDisabled = !!(!user || !user?.is_verified_author);
+
+  // Set data to locat storage
+  useEffect(() => {
+    localStorage.setItem('postData', JSON.stringify(postData));
+  }, [postData]);
 
   return (
     <PanelGroup direction="horizontal" className="create">
       <Panel defaultSize={45} minSize={20} className="create__panel">
+        <div className="create__tablist" role="tablist">
+          <button
+            type="button"
+            role="tab"
+            aria-selected={activePanel === 1}
+            aria-label="Show title and author panel"
+            aria-controls="create-panel__title"
+            id="title-tab"
+            className="create__tab"
+            data-active={activePanel === 1}
+            onClick={() => setActivePanel(1)}
+          >
+            Title
+          </button>
+
+          <button
+            type="button"
+            role="tab"
+            aria-selected={activePanel === 2}
+            aria-label="Show blog content body panel"
+            aria-controls="create-panel__body"
+            id="body-tab"
+            className="create__tab"
+            data-active={activePanel === 2}
+            onClick={() => setActivePanel(2)}
+          >
+            Body
+          </button>
+
+          <button
+            type="button"
+            role="tab"
+            aria-selected={activePanel === 3}
+            aria-label="Show panel to edit tags"
+            aria-controls="create-panel__tags"
+            id="tags-tab"
+            className="create__tab"
+            data-active={activePanel === 3}
+            onClick={() => setActivePanel(3)}
+          >
+            Tags
+          </button>
+
+          <button
+            type="button"
+            role="tab"
+            aria-selected={activePanel === 4}
+            aria-label="Show panel to edit display image details"
+            aria-controls="create-panel__display-img"
+            id="display-img-tab"
+            className="create__tab"
+            data-active={activePanel === 4}
+            onClick={() => setActivePanel(4)}
+          >
+            Display Image
+          </button>
+
+          <button
+            type="button"
+            role="tab"
+            aria-selected={activePanel === 5}
+            aria-label="Show panel to edit privacy and submit post"
+            aria-controls="create-panel__submit"
+            id="submit-tab"
+            className="create__tab"
+            data-active={activePanel === 5}
+            onClick={() => setActivePanel(5)}
+          >
+            Submit
+          </button>
+        </div>
+
         <form action="" className="create__form" onSubmit={handleSubmitPost}>
-          <label htmlFor="content-title" className="create__input-wrapper">
-            <span className="create__input-label">
-              Title<abbr title="required">*</abbr> :
-            </span>
-            <small className="create__input-sublabel">
-              Blog titles must be between 3 and 80 characters long.
-            </small>
-            <input
-              className="create__input"
-              id="content-title"
-              type="text"
-              name="title"
-              value={postData.title}
-              placeholder="Enter your title here"
-              minLength={3}
-              maxLength={80}
-              onChange={handleInfoChange}
-              required
-            />
-          </label>
+          <div
+            id="create-panel__title"
+            className="create__tabpanel"
+            role="tabpanel"
+            aria-labelledby="title-tab"
+            hidden={activePanel !== 1}
+          >
+            {/* TITLE */}
+            <label htmlFor="content-title" className="create__input-wrapper">
+              <span className="create__input-label">
+                Title<abbr title="required">*</abbr> :
+              </span>
+              <small className="create__input-sublabel">
+                Blog titles must be between 3 and 80 characters long.
+              </small>
+              <input
+                className="create__input"
+                id="content-title"
+                type="text"
+                name="title"
+                value={postData.title}
+                placeholder="Enter your title here"
+                minLength={3}
+                maxLength={80}
+                onChange={handleInfoChange}
+                required
+              />
+            </label>
+            {/* AUTHOR */}
+            <label htmlFor="content-author" className="create__input-wrapper">
+              <span className="create__input-label">
+                Author<abbr title="required">*</abbr> :
+              </span>
+              <input
+                className="create__input"
+                id="content-author"
+                type="text"
+                name="author"
+                value={postData.author}
+                placeholder="Enter your name here"
+                minLength={3}
+                maxLength={80}
+                onChange={handleInfoChange}
+                required
+              />
+            </label>
+          </div>
 
-          <label htmlFor="content-author" className="create__input-wrapper">
-            <span className="create__input-label">
-              Author<abbr title="required">*</abbr> :
-            </span>
-
-            <input
-              className="create__input"
-              id="content-author"
-              type="text"
-              name="author"
-              value={postData.author}
-              placeholder="Enter your name here"
-              minLength={3}
-              maxLength={80}
-              onChange={handleInfoChange}
-              required
-            />
-          </label>
-
-          <label htmlFor="content-markdown" className="create__input-wrapper">
-            <span className="create__input-label">
-              Body<abbr title="required">*</abbr> :
-            </span>
-            <small className="create__input-sublabel">
-              This editor uses Markdown to build the content of blog posts. If
-              you do not know how to use markdown syntax, see this guide.
-            </small>
-            <textarea
-              className="create__input textarea"
-              name="content"
-              id="content-markdown"
-              value={postData.content}
-              onChange={handleInfoChange}
-              placeholder="# Hello, *world*!"
-              minLength={6}
-              required
-            ></textarea>
-          </label>
-
-          {/* CATEGORY */}
-          <label htmlFor="content-category" className="create__input-wrapper">
-            <span className="create__input-label">
-              Category<abbr title="required">*</abbr> :
-            </span>
-            <select
-              className="create__input"
-              name="category"
-              id="content-category"
-              onChange={handleInfoChange}
-              value={postData.category}
-            >
-              <option value="">Choose a category</option>
-              {categories.map((cat) => (
-                <option
-                  value={cat}
-                  key={`option-${cat}`}
-                  className="create__input-option"
+          <div
+            className="create__tabpanel"
+            id="create-panel__body"
+            role="tabpanel"
+            aria-labelledby="body-tab"
+            hidden={activePanel !== 2}
+          >
+            {/* BODY */}
+            <label htmlFor="content-markdown" className="create__input-wrapper">
+              <span className="create__input-label">
+                Body<abbr title="required">*</abbr> :
+              </span>
+              <small className="create__input-sublabel">
+                This editor uses Markdown to build the content of blog posts. If
+                you do not know how to use Markdown,{' '}
+                <Link
+                  href="https://commonmark.org/help/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  style={{ textDecoration: 'underline' }}
                 >
-                  {cat.split('-').join(' ')}
-                </option>
-              ))}
-            </select>
-          </label>
+                  see this guide.
+                </Link>
+              </small>
+              <textarea
+                className="create__input textarea"
+                name="content"
+                id="content-markdown"
+                value={postData.content}
+                onChange={handleInfoChange}
+                placeholder="# Hello, *world*!"
+                minLength={6}
+                required
+              ></textarea>
+            </label>
+          </div>
 
-          {/* TAGS */}
-          <label htmlFor="create-tags" className="create__input-wrapper">
-            <span className="create__input-label">Tags:</span>
-            <small className="create__input-sublabel">
-              Press <code>Enter</code> to submit a tag for this post. Tags that
-              are not in any post yet are newly created.
-            </small>
-
-            {postData.tags.length > 0 && (
-              <ul className="create__tags-list">
-                {postData.tags.map((tag) => (
-                  <TagPreview
-                    tag={tag}
-                    deleteTag={deleteTag}
-                    key={`tag-pvw--${tag}`}
-                  />
+          <div
+            className="create__tabpanel"
+            id="create-panel__tags"
+            role="tabpanel"
+            aria-labelledby="tags-tab"
+            hidden={activePanel !== 3}
+          >
+            {/* CATEGORY */}
+            <label htmlFor="content-category" className="create__input-wrapper">
+              <span className="create__input-label">
+                Category<abbr title="required">*</abbr> :
+              </span>
+              <select
+                className="create__input"
+                name="category"
+                id="content-category"
+                onChange={handleInfoChange}
+                value={postData.category}
+              >
+                <option value="">Choose a category</option>
+                {categories.map((cat) => (
+                  <option
+                    value={cat}
+                    key={`option-${cat}`}
+                    className="create__input-option"
+                  >
+                    {cat.split('-').join(' ')}
+                  </option>
                 ))}
-              </ul>
-            )}
-
-            <input
-              id="create-tags"
-              className="create__input"
-              type="text"
-              name="currentTag"
-              placeholder="Asian Design"
-              value={postData.currentTag}
-              onChange={handleInfoChange}
-              onKeyDown={(e) => {
-                if (e.key !== 'Enter') return;
-                e.preventDefault();
-                addTag(e.currentTarget.value);
-              }}
-            />
-          </label>
+              </select>
+            </label>
+            {/* TAGS */}
+            <label htmlFor="create-tags" className="create__input-wrapper">
+              <span className="create__input-label">Tags:</span>
+              <small className="create__input-sublabel">
+                Press <code>Enter</code> to submit a tag for this post. Tags
+                that are not in any post yet are newly created.
+              </small>
+              {postData.tags.length > 0 && (
+                <ul className="create__tags-list">
+                  {postData.tags.map((tag) => (
+                    <TagPreview
+                      tag={tag}
+                      deleteTag={deleteTag}
+                      key={`tag-pvw--${tag}`}
+                    />
+                  ))}
+                </ul>
+              )}
+              <input
+                id="create-tags"
+                className="create__input"
+                type="text"
+                name="currentTag"
+                placeholder="Asian Design"
+                value={postData.currentTag}
+                onChange={handleInfoChange}
+                onKeyDown={(e) => {
+                  if (e.key !== 'Enter') return;
+                  e.preventDefault();
+                  addTag(e.currentTarget.value);
+                }}
+              />
+            </label>
+          </div>
 
           {/* DISPLAY IMAGE */}
-          <div>
+          <div
+            className="create__tabpanel"
+            id="create-panel__display-img"
+            role="tabpanel"
+            aria-labelledby="display-img-tab"
+            hidden={activePanel !== 4}
+          >
             {/* IMG URL */}
             <label htmlFor="create-img-url" className="create__input-wrapper">
               <span className="create__input-label">
@@ -298,34 +461,49 @@ function CreatePost() {
           </div>
 
           {/* IS PRIVATE */}
-          <div className="create__input-wrapper">
-            <label
-              htmlFor="create-post-privacy"
-              className="create__input-label"
+          <div
+            className="create__tabpanel"
+            id="create-panel__submit"
+            role="tabpanel"
+            aria-labelledby="submit-tab"
+            hidden={activePanel !== 5}
+          >
+            <div className="create__input-wrapper">
+              <label
+                htmlFor="create-post-privacy"
+                className="create__input-label"
+              >
+                Privacy:
+              </label>
+              <small className="create__input-sublabel">
+                Set post privacy. Defaults to false.
+              </small>
+              <Switch
+                isOn={postData.is_private || false}
+                id="create-post-privacy"
+                ariaLabel="Toggle Post Privacy"
+                handler={togglePrivacy}
+              />
+            </div>
+            <button
+              type="submit"
+              className="create__btn-submit"
+              disabled={isButtonDisabled}
             >
-              Privacy:
-            </label>
-            <small className="create__input-sublabel">
-              Set post privacy. Defaults to false.
-            </small>
-
-            <Switch
-              isOn={postData.is_private || false}
-              id="create-post-privacy"
-              ariaLabel="Toggle Post Privacy"
-              handler={togglePrivacy}
-            />
+              Submit Post
+            </button>
+            {isButtonDisabled && (
+              <p className="create__input-sublabel">
+                NOTE: You must be a verified author to submit a post
+              </p>
+            )}
           </div>
-
-          <button type="submit" className="create__btn-submit">
-            Submit Post
-          </button>
         </form>
       </Panel>
 
       <PanelResizeHandle className="create__resizer" />
 
-      <Panel className="create__panel">
+      <Panel className="create__panel preview">
         <PostPreview post={postData} />
       </Panel>
     </PanelGroup>
