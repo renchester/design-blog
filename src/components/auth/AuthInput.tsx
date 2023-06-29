ba8756a2f59a21401fdffa28c14655eae512e0e6
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-29 18:01:47 +0800

    Replace material icons with SVG

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -53,15 +53,38 @@ function AuthInput(props: AuthInputProps) {
           isValid !== undefined &&
           (isValid ? (
             <div className="auth-validation" aria-label={`${label} is valid`}>
-              <i className="material-symbols-outlined auth-validation__icon auth-validation__valid-icon">
-                check
-              </i>
+              <svg
+                className="auth-validation__icon auth-validation__valid-icon"
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <polyline points="20 6 9 17 4 12"></polyline>
+              </svg>
             </div>
           ) : (
             <div className="auth-validation" aria-label={`${label} is invalid`}>
-              <i className="material-symbols-outlined auth-validation__icon auth-validation__invalid-icon">
-                close
-              </i>
+              <svg
+                className="auth-validation__icon auth-validation__invalid-icon"
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18"></line>
+                <line x1="6" y1="6" x2="18" y2="18"></line>
+              </svg>
             </div>
           ))}
       </div>
