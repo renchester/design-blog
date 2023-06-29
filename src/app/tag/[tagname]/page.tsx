c6abb71e752d5f858fa68031531967d803a35432
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-29 17:51:51 +0800

    Create TagPage component and styles

---
+++ src/app/tag/[tagname]/page.tsx
@@ -0,0 +1,53 @@
+import './TagPage.scss';
+import SidePreview from '@/components/blogPreview/SidePreview';
+import axios from '@/lib/axios';
+import { BlogPost } from '@/types/types';
+import unescapeBlogPost from '@/utils/unescapers/unescapeBlogPost';
+import { Metadata } from 'next';
+
+type TagPageProps = {
+  params: {
+    tagname: string;
+  };
+};
+
+const getBlogsByTagname = async (tagname: string) => {
+  const response = await axios.get(`/api/posts/tag/${tagname}`);
+  const posts = response.data.posts as BlogPost[];
+  const formattedPosts = posts.map((post) => post && unescapeBlogPost(post));
+
+  return formattedPosts;
+};
+
+export async function generateMetadata(props: TagPageProps): Promise<Metadata> {
+  const { tagname } = props.params;
+
+  return {
+    title: `Tag - ${tagname}`,
+  };
+}
+
+async function TagPage(props: TagPageProps) {
+  const { tagname } = props.params;
+  const posts = await getBlogsByTagname(tagname);
+
+  return (
+    <main className="tag-page">
+      <h1 className="tag-page__title"> #{tagname}</h1>
+      {posts.length > 0 ? (
+        <ul className="tag-page__list">
+          {posts.map((post) => (
+            <SidePreview key={`tag__${post._id}`} blog={post} />
+          ))}
+        </ul>
+      ) : (
+        <div className="tag-page__empty">
+          <p className="tag-page__quiet">
+            It&apos;s a bit too quiet in here...
+          </p>
+        </div>
+      )}
+    </main>
+  );
+}
+export default TagPage;
