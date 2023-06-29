5fc2b8ac5f9b775f6c8a5523df72463f8fa384fa
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-29 17:51:31 +0800

    Render dynamic content within PostPage

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,17 +1,18 @@
-import './PostPage.scss';
+import '@/styles/_post.scss';
 import Link from 'next/link';
+import ReactMarkdown from 'react-markdown';
 import unescape from 'validator/lib/unescape';
 import { format } from 'date-fns';
 import { Metadata } from 'next';
 
-import { CommentProvider } from '@/context/CommentContext';
 import axios from '@/lib/axios';
 import { BlogPost } from '@/types/types';
+import { CommentProvider } from '@/context/CommentContext';
 
-import sampleBlogPost from '@/data/sampleBlogPost';
 import SmallPreview from '@/components/blogPreview/SmallPreview';
 import CommentFeed from '@/components/comments/CommentFeed';
 import LikePostButton from '@/components/buttons/LikePostButton';
+import unescapeBlogPost from '@/utils/unescapers/unescapeBlogPost';
 
 type PostPageProps = {
   params: {
@@ -21,8 +22,17 @@ type PostPageProps = {
 
 const getBlogPost = async (slug: string) => {
   const response = await axios.get(`/api/posts/${slug}`);
-  const post = response.data.post;
-  return post;
+  const post = response.data.post as BlogPost;
+
+  return unescapeBlogPost(post);
+};
+
+const getTrendingPosts = async () => {
+  const response = await axios.get('/api/posts?limit=4');
+  const posts = response.data.posts as BlogPost[];
+  const formattedPosts = posts.map((post) => unescapeBlogPost(post));
+
+  return formattedPosts;
 };
 
 export async function generateMetadata(
@@ -34,47 +44,44 @@ export async function generateMetadata(
   const blog = response.data.post;
 
   return {
-    title: blog.title,
+    title: unescape(blog.title),
   };
 }
 
-// export async function generateStaticParams() {
-//   const response = await axios.get('/api/posts');
-//   const posts = response.data.posts as BlogPost[];
+export async function generateStaticParams() {
+  const response = await axios.get('/api/posts');
+  const posts = response.data.posts as BlogPost[];
 
-//   return posts.map((post) => ({
-//     slug: post.slug,
-//   }));
-// }
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
 
-function PostPage(props: PostPageProps) {
+async function PostPage(props: PostPageProps) {
   const { slug } = props.params;
-  // const post = (await getBlogPost(slug)) as BlogPost;
-  const post = sampleBlogPost;
+  const post = await getBlogPost(slug);
+  const trendingPosts = await getTrendingPosts();
 
   const formattedDate = format(new Date(post.date_created), 'LL.dd.yy');
 
   return (
-    <div className="post-page">
+    <div className="post">
       {/* META */}
-      <article className="post-page__main">
-        <Link
-          href={`/category/${post.category}`}
-          className="post-page__category"
-        >
+      <article className="post__main">
+        <Link href={`/category/${post.category}`} className="post__category">
           {post.category}
         </Link>
 
-        <h1 className="post-page__title">{post.title}</h1>
-        <div className="post-page__meta">
+        <h1 className="post__title">{post.title}</h1>
+        <div className="post__meta">
           <time
             dateTime={post.date_created.toString()}
-            className="post-page__meta-date"
+            className="post__meta-date"
           >
             {formattedDate}
           </time>{' '}
           |
-          <span className="post-page__meta-info">
+          <span className="post__meta-info">
             {' '}
             by{' '}
             <Link href={`/user/${post.author.username}`}>
@@ -85,38 +92,37 @@ function PostPage(props: PostPageProps) {
       </article>
 
       {/* DISPLAY IMG */}
-      <article className="post-page__display">
+      <article className="post__display">
         <img
           src={post.display_img.url}
           alt="Cover image"
-          className="post-page__display-img"
+          className="post__display-img"
         />
-        <span className="post-page__display-meta">
+        <span className="post__display-meta">
           Photo by {post.display_img.owner} from {post.display_img.source}
         </span>
       </article>
 
       {/* MAIN */}
-      <main className="post-page__content">
-        {unescape(post.content)}
-        {/* like post  */}
+      <main className="post__content">
+        <ReactMarkdown>{post.content}</ReactMarkdown>
       </main>
 
-      <div className="post-page__like-post">
+      <div className="post__like-post">
         <LikePostButton blog={post} />
       </div>
 
       {/* TAGS */}
       {post.tags.length > 0 && (
-        <section className="post-page__tags">
-          <h3 className="post-page__tags-title" id="post-page__tags-title">
+        <section className="post__tags">
+          <h3 className="post__tags-title" id="post__tags-title">
             Tags
           </h3>
 
-          <ul className="post-page__tags-list">
+          <ul className="post__tags-list">
             {post.tags.map((tag) => (
-              <li key={`tag-${tag._id}`} className="post-page__tags-item">
-                <Link href={`/tag/${tag.name}`}>{tag.name}</Link>
+              <li key={`post-tag-${tag}`} className="post__tags-item">
+                <Link href={`/tag/${tag}`}>{tag}</Link>
               </li>
             ))}
           </ul>
@@ -125,14 +131,11 @@ function PostPage(props: PostPageProps) {
 
       {/* COMMENTS */}
       <section
-        className="post-page__comments"
-        aria-labelledby="post-page__comments-title"
+        className="post__comments"
+        aria-labelledby="post__comments-title"
       >
         <CommentProvider parentPost={post}>
-          <h3
-            id="post-page__comments-title"
-            className="post-page__comments-title"
-          >
+          <h3 id="post__comments-title" className="post__comments-title">
             Comments
           </h3>
           <CommentFeed comments={post.comments} />
@@ -141,29 +144,19 @@ function PostPage(props: PostPageProps) {
 
       {/* FEATURED / LINKS TO OTHER POSTS */}
       <section
-        className="post-page__featured"
-        aria-labelledby="post-page__featured-title"
+        className="post__featured"
+        aria-labelledby="post__featured-title"
       >
-        <h3
-          id="post-page__featured-title"
-          className="post-page__featured-title"
-        >
+        <h3 id="post__featured-title" className="post__featured-title">
           Trending Posts
         </h3>
 
-        <ul className="post-page__featured-list">
-          <li className="post-page__featured-item">
-            <SmallPreview blog={post} />
-          </li>
-          <li className="post-page__featured-item">
-            <SmallPreview blog={post} />
-          </li>
-          <li className="post-page__featured-item">
-            <SmallPreview blog={post} />
-          </li>
-          <li className="post-page__featured-item">
-            <SmallPreview blog={post} />
-          </li>
+        <ul className="post__featured-list">
+          {trendingPosts.map((post) => (
+            <li className="post__featured-item" key={`trending-${post.slug}`}>
+              <SmallPreview blog={post} />
+            </li>
+          ))}
         </ul>
       </section>
     </div>
