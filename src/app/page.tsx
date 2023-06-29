305b0fff96a29488ff612f0389279bf80cc9c8f5
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-29 17:50:39 +0800

    Add function to call API for HomePage dynamic content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,34 @@
 import './HomePage.scss';
 import { Metadata } from 'next';
 
-import sampleBlogPost from '@/data/sampleBlogPost';
+import axios from '@/lib/axios';
 import Hero from '@/components/collections/Hero';
 import FeaturedTags from '@/components/collections/FeaturedTags';
 import FeaturedVideo from '@/components/collections/FeaturedVideo';
 import PostColumn from '@/components/collections/PostColumn';
 import CommunitySection from '@/components/collections/CommunitySection';
 import CallToAction from '@/components/collections/CallToAction';
+import { BlogPost } from '@/types/types';
+import unescapeBlogPost from '@/utils/unescapers/unescapeBlogPost';
+import Link from 'next/link';
+
+const getBlogPosts = async () => {
+  const response = await axios.get(`/api/posts`);
+  const posts = response.data.posts as BlogPost[];
+  const formattedPosts = posts.map((post) => unescapeBlogPost(post));
+
+  return formattedPosts;
+};
 
 export const metadata: Metadata = {
   title: 'Dezien',
   description: 'A collective for architecture, tech, design, and more.',
 };
 
-export default function Home() {
-  const sampleBlogs = [sampleBlogPost, sampleBlogPost, sampleBlogPost];
+export default async function Home() {
+  const blogs = await getBlogPosts();
+  const firstHalf = blogs.slice(0, 5);
+  const secondHalf = blogs.slice(5);
 
   return (
     <main className="home-page">
@@ -28,15 +41,21 @@ export default function Home() {
       </div>
 
       <div className="home-page__section">
-        <PostColumn blogs={sampleBlogs} />
+        <PostColumn blogs={firstHalf} />
       </div>
 
       <div className="home-page__section-full">
-        <FeaturedTags tags={sampleBlogPost.tags} />
+        <FeaturedTags tags={blogs[1].tags} />
       </div>
 
       <div className="home-page__section">
-        <PostColumn blogs={sampleBlogs} />
+        <PostColumn blogs={secondHalf} />
+      </div>
+
+      <div className="home-page__section">
+        <Link href="/latest" className="home-page__all">
+          See All Posts
+        </Link>
       </div>
 
       <div className="home-page__section">
