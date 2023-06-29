219cc4f1bc08d1486ba4b3eb4a4f1d79ba5b2553
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-29 17:54:17 +0800

    Add dynamic content to Hero collection

--- src/components/collections/Hero.tsx
+++ src/components/collections/Hero.tsx
@@ -1,58 +1,33 @@
 import { BlogPost } from '@/types/types';
 import HeroPreview from '../blogPreview/HeroPreview';
 import './Hero.scss';
-import Link from 'next/link';
 import axios from '@/lib/axios';
+import unescapeBlogPost from '@/utils/unescapers/unescapeBlogPost';
 
-const getPost = async () => {
-  const response = await axios.get('/api/posts');
-  const post = response.data.posts[0];
-
-  return post;
+const getBlogPosts = async () => {
+  const response = await axios.get('/api/posts?limit=5');
+  const posts = response.data.posts as BlogPost[];
+  const formattedPosts = posts.map((post) => unescapeBlogPost(post));
+  return formattedPosts;
 };
 
 async function Hero() {
-  // const sampleBlog: BlogPost = {
-  //   _id: '1',
-  //   content:
-  //     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque quae corporis ipsum, non totam eveniet ratione sit ullam dignissimos voluptates veniam. Quos, quisquam maxime necessitatibus corrupti consequuntur veritatis similique sed.',
-  //   title: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-  //   date_created: new Date(),
-  //   category: 'lifestyle',
-  //   author: {
-  //     _id: 'author-1',
-  //     first_name: 'Renchester',
-  //     last_name: 'Ramos',
-  //     username: 'rkahn',
-  //     email: 'r@gmail.com',
-  //     is_admin: true,
-  //   },
-  //   editors: [],
-  //   slug: 'sample-title',
-  //   liked_by: [],
-  //   edits: [],
-  //   is_private: false,
-  //   comments: [],
-  //   tags: [],
-  // };
-
-  const sampleBlog = (await getPost()) as BlogPost;
-  const blogString = JSON.stringify(sampleBlog);
+  const blogPosts = await getBlogPosts();
 
   return (
     <section className="hero">
       <div className="hero__col-side">
-        <HeroPreview blog={sampleBlog} />
-        <HeroPreview blog={sampleBlog} />
+        <HeroPreview blog={blogPosts[1]} />
+        <HeroPreview blog={blogPosts[2]} />
       </div>
 
       <div className="hero__col-main">
-        <HeroPreview blog={sampleBlog} isMain />
+        <HeroPreview blog={blogPosts[0]} isMain />
       </div>
 
       <div className="hero__col-side">
-        <HeroPreview blog={sampleBlog} />
-        <HeroPreview blog={sampleBlog} />
+        <HeroPreview blog={blogPosts[3]} />
+        <HeroPreview blog={blogPosts[4]} />
       </div>
     </section>
   );
